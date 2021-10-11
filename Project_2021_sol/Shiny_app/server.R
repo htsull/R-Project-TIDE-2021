579349550a23950fbe23a873a9917cda46d4ab23
Berthony Sully <berthonysully@gmail.com>
2021-10-11 13:39:10 +0200

    revert to 8 days ago when no interruption

--- Project_2021_sol/Shiny_app/server.R
+++ Project_2021_sol/Shiny_app/server.R
@@ -11,7 +11,7 @@ server <- function(input, output, session) {
   
   titleChoices <- reactive({
   
-    data_$original_title[data_$decade == input$decade] %>% sort
+    data_$title[data_$decade == input$decade] %>% sort
     
   })
   
@@ -23,36 +23,36 @@ server <- function(input, output, session) {
     
     table(original_title = input$title,
           
-          year = data_$year[data_$decade == input$decade & data_$original_title == input$title],
+          year = data_$year[data_$decade == input$decade & data_$title == input$title],
           
-          director = data_$director[data_$decade == input$decade & data_$original_title == input$title],
+          director = data_$director[data_$decade == input$decade & data_$title == input$title],
           
-          genre = data_$genre[data_$decade == input$decade & data_$original_title == input$title],
+          genre = data_$genre[data_$decade == input$decade & data_$title == input$title],
           
-          writer = data_$writer[data_$decade == input$decade & data_$original_title == input$title],
+          writer = data_$writer[data_$decade == input$decade & data_$title == input$title],
           
-          duration = data_$duration[data_$decade == input$decade & data_$original_title == input$title],
+          duration = data_$duration[data_$decade == input$decade & data_$title == input$title],
           
-          production_company = data_$production_company[data_$decade == input$decade & data_$original_title == input$title],
+          production_company = data_$production_company[data_$decade == input$decade & data_$title == input$title],
           
-          country = data_$country[data_$decade == input$decade & data_$original_title == input$title],
+          country = data_$country[data_$decade == input$decade & data_$title == input$title],
           
-          actors = data_$actors[data_$decade == input$decade & data_$original_title == input$title],
+          actors = data_$actors[data_$decade == input$decade & data_$title == input$title],
           
-          language = data_$language[data_$decade == input$decade & data_$original_title == input$title],
+          language = data_$language[data_$decade == input$decade & data_$title == input$title],
           
-          avg_vote = data_$avg_vote[data_$decade == input$decade & data_$original_title == input$title],
+          avg_vote = data_$avg_vote[data_$decade == input$decade & data_$title == input$title],
           
-          budget = data_$budget[data_$decade == input$decade & data_$original_title == input$title],
+          budget = data_$budget[data_$decade == input$decade & data_$title == input$title],
           
-          description = data_$description[data_$decade == input$decade & data_$original_title == input$title]
+          description = data_$description[data_$decade == input$decade & data_$title == input$title]
           )
   })
 
 # output of the findings length afer user input filter
   output$founds <- reactive({
     
-    paste(data_$original_title[data_$decade == input$decade] %>% length(), "result(s) found")
+    paste(data_$title[data_$decade == input$decade] %>% length(), "result(s) found")
     
   })
   
@@ -64,7 +64,8 @@ out <-  data_ %>%
            Language = language,
            Director = director,
            Writer = writer,
-           ` Prod. Company` = production_company) %>% head()
+           ` Prod. Company` = production_company) %>% 
+    head(50)
   
   output$data_view <- renderDataTable(
     out,
@@ -74,6 +75,21 @@ out <-  data_ %>%
                    searching= T
                    )
   )
+  
+  
+  observeEvent(
+    input$tab_fil1,
+    updateSelectInput(session,
+                      inputId = "tab_fil2",
+                      label = paste("Choose the", tolower(input$tab_fil1) ,"below"),
+                      choices = titleChoices()
+    )
+  )
+  
+  
+  
+  
+  
   
   
 
@@ -99,7 +115,12 @@ out <-  data_ %>%
 #                     choices = uniLang())
 #   )
 # 
-
+# uniLang <- reactive({
+#   lang1 <- data_$language[grepl("French", data_$language)]
+#   lang2 <- paste(lang1, collapse = ',')
+#   lang3 <- strsplit(lang2, ",")
+#   lang4 <- lang3[[1]] %>% unique %>% sort() %>%  as.list()
+# })
 
 #to use fo language filter
 # testLang <- data_$language[grepl("French", data_$language)] %>%
@@ -164,45 +185,3 @@ out <-  data_ %>%
 
   
   
-  # 
-  # filter_by <- reactive({
-  #   if(tolower(input$tab_fil1) == "actor"){
-  #     lang1 <- data_$actors
-  #     lang2 <- paste(lang1, collapse = ',')
-  #     lang3 <- strsplit(lang2, ",")
-  #     lang4 <- lang3[[1]] %>% unique %>% sort() %>%  as.list()
-  #     return(lang4)
-  #     
-  #   }else if(tolower(input$tab_fil1) == "language"){
-  #     lang1 <- data_$language[grepl(input$tab_fil1, data_$language)]
-  #     lang2 <- paste(lang1, collapse = ',')
-  #     lang3 <- strsplit(lang2, ",")
-  #     lang4 <- lang3[[1]] %>% unique %>% sort() %>%  as.list()
-  #     return(lang4) 
-  #     
-  #   }else if(tolower(input$tab_fil1) == "genre"){
-  #     lang1 <- data_$genre[grepl(input$tab_fil1, data_$genre)]
-  #     lang2 <- paste(lang1, collapse = ',')
-  #     lang3 <- strsplit(lang2, ",")
-  #     lang4 <- lang3[[1]] %>% unique %>% sort() %>%  as.list()
-  #     return(lang4)      
-  #   }
-  #   
-  # })
-  
-  
-  
-
-    # 
-  # data_$original_title[grepl('Action', data_$genre)] %>% length()
-  # 
-  # data_$actors[grepl(input$tab_fil1, data_$actors)]
-  
-  # observeEvent(
-  #   input$tab_fil1,
-  #   updateSelectInput(session,
-  #                     inputId = "tab_fil2",
-  #                     label = paste("Choose the", tolower(input$tab_fil1) ,"below"),
-  #                     choices = filter_by()
-  #   )
-  # )
