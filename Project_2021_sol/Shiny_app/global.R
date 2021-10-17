718006733e7ef16375db48a3a37a8b58445e162d
Berthony Sully <berthonysully@gmail.com>
2021-10-17 15:57:42 +0200

    cleaning up

--- Project_2021_sol/Shiny_app/global.R
+++ Project_2021_sol/Shiny_app/global.R
@@ -6,6 +6,10 @@ path_ <- "./data_shiny_useable.csv"
 data_ <- read.csv(file = path_, sep = ",", header = T, encoding = "uft-8")
 
 
+
+
+
+
 decade_val <- unique(data_$decade) %>%  sort %>% as.list
 data_$language[data_$language == "None"] <- NA
 
